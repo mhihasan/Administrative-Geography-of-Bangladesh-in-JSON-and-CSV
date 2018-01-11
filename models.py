214c4010ff459bb7db1cc73249e171c43eed0f97
Hasanul Islam <hasanuli10@gmail.com>
2018-01-12 01:46:09 +0600

    Administrative geography added

---
+++ models.py
@@ -0,0 +1,17 @@
+class Division(models.Model):
+    name = models.CharField(max_length=15)
+
+
+class District(models.Model):
+    name = models.CharField(max_length=20)
+    division = models.ForeignKey(Division)
+
+
+class SubDistrict(models.Model):
+    name = models.CharField(max_length=30)
+    district = models.ForeignKey(District)
+
+
+class Union(models.Model):
+    name = models.CharField(max_length=50)
+    sub_district = models.ForeignKey(SubDistrict)
